11b6f892260f753f469db9232d2ff382d586d786
Kendrick Lopez <20200165@ricaldone.edu.sv>
2024-08-31 20:10:06 -0600

    Subi pantallas

---
+++ HATS_EMPS_PTC2024/app/src/main/java/hats/emps_ptc2024/ui/home/HomeViewModel.kt
@@ -0,0 +1,13 @@
+package hats.emps_ptc2024.ui.home
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class HomeViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is home Fragment"
+    }
+    val text: LiveData<String> = _text
+}
---
+++ HATS_EMPS_PTC2024/app/src/main/java/hats/emps_ptc2024/ui/notifications/NotificationsViewModel.kt
@@ -0,0 +1,13 @@
+package hats.emps_ptc2024.ui.notifications
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class NotificationsViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is notifications Fragment"
+    }
+    val text: LiveData<String> = _text
+}
